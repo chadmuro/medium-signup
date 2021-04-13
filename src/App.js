dd632d2cf9bca5a0ff5cfbed959b5296c16089f9
chadmuro <63533886+chadmuro@users.noreply.github.com>
2021-04-13 22:33:17 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import ModalDialog from './ModalDialog';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [open, setOpen] = useState(false);
+
+	const handleOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	return (
+		<div className="App">
+			<Button variant="contained" color="primary" onClick={handleOpen}>
+				Signup
+			</Button>
+			<ModalDialog open={open} handleClose={handleClose} />
+		</div>
+	);
+};
 
 export default App;
