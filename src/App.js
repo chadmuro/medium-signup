5be431698b602bf2bb23e23b597bf110f55b981f
chadmuro <63533886+chadmuro@users.noreply.github.com>
2021-05-03 13:08:31 +0900

    features section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './Navbar';
 import Hero from './Hero';
+import Features from './Features';
 
 const App = () => {
 	return (
 		<div className="App">
 			<Navbar />
 			<Hero />
+			<Features />
 		</div>
 	);
 };
